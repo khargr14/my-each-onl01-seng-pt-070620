5b389378cd0689f87cd20cf12d721628bb399783
Keesha Hargrove <kharg14@gmail.com>
2020-07-14 14:01:08 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
-end
+function iterativeLog(array){
+  array.forEach((item, index) => {
+    console.log(index + ": " + item)
+  });
+}
+
+function iterate(callback) {
+  var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  array.forEach(callback);
+  return array
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback);
